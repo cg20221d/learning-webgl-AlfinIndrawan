beb3f2448670c252fb69206cf4dcf05dc268b5eb
Alfin Indrawan <alfinindrawan54@gmail.com>
2022-09-20 17:19:06 +0700

    digit 9 pertama

--- main.js
+++ main.js
@@ -2,20 +2,44 @@ function main() {
   var kanvas = document.getElementById("kanvas");
   // gl sebagai alat pointer gambar
   var gl = kanvas.getContext("webgl");
+  // create a vertices that make word A
+  var vertices = [
+    -0.5, -0.5,
+    -0.25, -0.5,
+    -0.25, -0.5,
+    0, -0.5,
+    0, -0.5,
+    0, -0.25,
+    0, -0.25,
+    0, 0,
+    0.0, 0.0,
+    -0.5, 0,
+    -0.5, 0,
+    -0.5, 0.5,
+    0, 0.5,
+    - 0.5, 0.5,
+    0, 0.5,
+    0, 0
+  ];
+  var vertices_buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertices_buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
   // vertex shadder
   var vertexShaderCode =
-    `void main() { 
-      float x = 0.0;
-      float y = 0.0;
-      gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-      gl_PointSize = 100.0;
+    `
+    attribute vec2 aPosition;
+    void main() { 
+      float x = aPosition.x;
+      float y = aPosition.y;
+      gl_Position = vec4(x, y , 0.0, 1.0);
+      gl_PointSize = 5.0;
       }`;
   var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShaderObject, vertexShaderCode);
   gl.compileShader(vertexShaderObject); // object
   // fragment shader
   var fragmentShaderCode = `
-  precision mediump float;
+  precision highp float;
     void main() {
       float r = 0.0;
       float g = 0.0;
@@ -31,8 +55,13 @@ function main() {
   gl.attachShader(shaderProgram, fragmentShaderObject);
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
+  // teaching gpu how to read position from array_buffer to each vertex that processed
+  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
   // oranye
   gl.clearColor(1.0, 0.5, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
+  // draw a  9
+  gl.drawArrays(gl.LINES, 0, 16);
 }
