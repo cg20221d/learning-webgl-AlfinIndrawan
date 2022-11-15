4daebaa07d01c6e67ef209c0d13fd06ded12f4fc
Alfin Indrawan <alfinindrawan54@gmail.com>
2022-11-15 22:50:43 +0700

    finish number 4,5 and progressing number 6 and 7 mockup for alphabet

--- scripts/main.js
+++ scripts/main.js
@@ -3,25 +3,55 @@ function main() {
   var gl = kanvas.getContext("webgl");
 
   var vertices9_1 = [
-    // White
-    -0.35, 0.4, 0.0, 1, 1, 1,    // Index:  0
-    -0.7, 0.4, 0.0, 1, 1, 1,    // Index:  1
-    -0.8, 0.4, 0.0, 1, 1, 1,    // Index:  2
-    -0.8, 0.7, 0.0, 1, 1, 1,    // Index:  3
-    -0.7, 0.8, 0.0, 1, 1, 1,    // Index:  4
-    -0.35, 0.8, 0.0, 1, 1, 1,    // Index:  5
-    -0.25, 0.8, 0.0, 1, 1, 1,    // Index:  6
-    -0.25, 0.25, 0.0, 1, 1, 1,    // Index:  7
-    -0.35, 0.15, 0.0, 1, 1, 1,    // Index:  8
-    -0.7, 0.15, 0.0, 1, 1, 1,    // Index:  9
-    -0.8, 0.15, 0.0, 1, 1, 1,    // Index:  10
-    -0.8, 0.35, 0.0, 1, 1, 1,    // Index:  11
-    -0.7, 0.35, 0.0, 1, 1, 1,    // Index:  12
-    -0.7, 0.3, 0.0, 1, 1, 1,    // Index:  13
-    -0.65, 0.25, 0.0, 1, 1, 1,    // Index:  14
-    -0.4, 0.25, 0.0, 1, 1, 1,    // Index:  15
-    -0.35, 0.3, 0.0, 1, 1, 1,    // Index:  16
+    -0.35, 0.4, 0.1, 1, 1, 1,    // Index:  0
+    -0.7, 0.4, 0.1, 1, 1, 1,    // Index:  1
+    -0.8, 0.4, 0.1, 1, 1, 1,    // Index:  2
+    -0.8, 0.7, 0.1, 1, 1, 1,    // Index:  3
+    -0.7, 0.8, 0.1, 1, 1, 1,    // Index:  4
+    -0.35, 0.8, 0.1, 1, 1, 1,    // Index:  5
+    -0.25, 0.8, 0.1, 1, 1, 1,    // Index:  6
+    -0.25, 0.25, 0.1, 1, 1, 1,    // Index:  7
+    -0.35, 0.15, 0.1, 1, 1, 1,    // Index:  8
+    -0.7, 0.15, 0.1, 1, 1, 1,    // Index:  9
+    -0.8, 0.15, 0.1, 1, 1, 1,    // Index:  10
+    -0.8, 0.35, 0.1, 1, 1, 1,    // Index:  11
+    -0.7, 0.35, 0.1, 1, 1, 1,    // Index:  12
+    -0.7, 0.3, 0.1, 1, 1, 1,    // Index:  13
+    -0.65, 0.25, 0.1, 1, 1, 1,    // Index:  14
+    -0.4, 0.25, 0.1, 1, 1, 1,    // Index:  15
+    -0.35, 0.3, 0.1, 1, 1, 1,    // Index:  16
   ];
+
+  var verticesA = [
+    // create 'A' shape with 3D effect 
+    -0.35, 0.4, 0.1, 1, 1, 1,    // Index:  0
+    -0.7, 0.4, 0.1, 1, 1, 1,    // Index:  1
+    -0.8, 0.4, 0.1, 1, 1, 1,    // Index:  2
+    -0.8, 0.7, 0.1, 1, 1, 1,    // Index:  3
+    -0.7, 0.8, 0.1, 1, 1, 1,    // Index:  4
+    -0.35, 0.8, 0.1, 1, 1, 1,    // Index:  5
+    -0.25, 0.8, 0.1, 1, 1, 1,    // Index:  6
+    -0.25, 0.25, 0.1, 1, 1, 1,    // Index:  7
+    -0.35, 0.15, 0.1, 1, 1, 1,    // Index:  8
+    -0.7, 0.15, 0.1, 1, 1, 1,    // Index:  9
+    -0.8, 0.15, 0.1, 1, 1, 1,    // Index:  10
+    -0.8, 0.35, 0.1, 1, 1, 1,    // Index:  11
+    -0.7, 0.35, 0.1, 1, 1, 1,    // Index:  12
+    -0.7, 0.3, 0.1, 1, 1, 1,    // Index:  13
+    -0.65, 0.25, 0.1, 1, 1, 1,    // Index:  14
+    -0.4, 0.25, 0.1, 1, 1, 1,    // Index:  15
+    -0.35, 0.3, 0.1, 1, 1, 1,    // Index:  16
+  ];
+
+
+  //   // "N"
+  //   0.5, -0.4,
+  //   0.5, 0.4,
+  //   0.5, 0.4,
+  //   0.9, -0.4,
+  //   0.9, -0.4,
+
+
 
   var indices9_1 = [
     0, 1, 2, 0, 2, 3,
@@ -32,12 +62,27 @@ function main() {
     20, 21, 22, 20, 22, 23,
   ];
 
+  var indicesA = [
+    0, 1, 2, 0, 2, 3,
+    4, 5, 6, 4, 6, 7,
+    8, 9, 10, 8, 10, 11,
+    12, 13, 14, 12, 14, 15,
+    16, 17, 18, 16, 18, 19,
+    20, 21, 22, 20, 22, 23,
+  ];
 
   var objects = [
     {
       name: '9',
       vertices: vertices9_1,
       indices: indices9_1,
+      length: 16,
+      type: gl.LINE_LOOP,
+    },
+    {
+      name: 'A',
+      vertices: verticesA,
+      indices: indicesA,
       length: 16,
       type: gl.LINE_LOOP,
     }
@@ -137,7 +182,7 @@ function main() {
     gl.drawElements(glType, indices.length - 11, gl.UNSIGNED_SHORT, 0);
   }
 
-  const animatemove9 = () => {
+  const animateMove9 = () => {
     var model = mat4.create();
 
     if (horizontalDelta >= (frameWidth / 2) || horizontalDelta <= (-frameWidth / 2 + 1)) {
@@ -153,6 +198,55 @@ function main() {
     gl.uniformMatrix4fv(uView, false, view);
     gl.uniformMatrix4fv(uProjection, false, perspective);
     drawing(objects[0].vertices, objects[0].indices, 0, objects[0].length, objects[0].type);
+  }
+
+  const animateSize9 = () => {
+    var model = mat4.create();
+
+    if (scaleDelta >= 0.5 || scaleDelta <= -0.5) {
+      scaleSpeed = scaleSpeed * -1;
+    }
+
+    scaleDelta += scaleSpeed;
+    mat4.scale(model, model, [1.0 + scaleDelta, 1.0 + scaleDelta, 1.0 + scaleDelta]);
+
+    var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+    var uView = gl.getUniformLocation(shaderProgram, "uView");
+    var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+    gl.uniformMatrix4fv(uModel, false, model);
+    gl.uniformMatrix4fv(uView, false, view);
+    gl.uniformMatrix4fv(uProjection, false, perspective);
+    drawing(objects[0].vertices, objects[0].indices, 0, objects[0].length, objects[0].type);
+  }
+
+  const animateRotationA = () => {
+    // Rotate the vertices 'A' object about the Y axis with -1 angular velocity
+    var model = mat4.create();
+    mat4.rotateY(model, model, theta);
+    theta += 0.01;
+
+    var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+    var uView = gl.getUniformLocation(shaderProgram, "uView");
+    var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+    gl.uniformMatrix4fv(uModel, false, model);
+    gl.uniformMatrix4fv(uView, false, view);
+    gl.uniformMatrix4fv(uProjection, false, perspective);
+    drawing(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
+  }
+
+  const animateRotationN = () => {
+    // Rotate the vertices 'A' object about the X axis with -1 angular velocity
+    var model = mat4.create();
+    mat4.rotateX(model, model, theta);
+    theta += 0.01;
+
+    var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+    var uView = gl.getUniformLocation(shaderProgram, "uView");
+    var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+    gl.uniformMatrix4fv(uModel, false, model);
+    gl.uniformMatrix4fv(uView, false, view);
+    gl.uniformMatrix4fv(uProjection, false, perspective);
+    drawing(objects[1].vertices, objects[1].indices, 0, objects[1].length, objects[1].type);
   }
 
   var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
@@ -193,8 +287,10 @@ function main() {
     gl.uniformMatrix4fv(uModel, false, model);
     gl.uniformMatrix4fv(uView, false, view);
     gl.uniformMatrix4fv(uProjection, false, perspective);
-    // gl.drawElements(gl.LINE_LOOP, indices.length, gl.UNSIGNED_SHORT, 0);
-    animatemove9();
+    animateMove9();
+    animateSize9();
+    animateRotationA();
+    animateRotationN();
     requestAnimationFrame(render);
   }
   requestAnimationFrame(render);
